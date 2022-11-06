8b337a2181e5bba5fc2ba16ff43503c1b55b3b62
gchaha17 <chahalgagan8993@gmail.com>
2022-11-05 17:34:50 -0700

    second database commit

---
+++ src/main/java/com/example/getmesocialservice/resource/UserResource.java
@@ -0,0 +1,67 @@
+package com.example.getmesocialservice.resource;
+
+
+import com.example.getmesocialservice.model.Album;
+import com.example.getmesocialservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/albums")
+public class UserResource {
+    @Autowired
+    private UserService u2;
+    @PostMapping
+    public Album saveAlbum(@RequestBody Album al)
+    {
+
+        return u2.saveAlbum(al);
+
+    }
+    @GetMapping
+    public List<Album> getAllAlbum() {
+        return u2.getAllAlbum();
+
+    }
+    @PutMapping
+    public Album updateAlbum(@RequestBody Album al)
+    {
+
+        return u2.updateAlbum(al);
+    }
+    @DeleteMapping
+    public void deleteAlbum(@RequestParam(name="albumId") String albumId)
+    {
+        u2.deleteAlbum(albumId);
+
+    }
+    @GetMapping("/find")
+    public List<Album> getalbumByName(@RequestParam (name="name") String name)
+    {
+
+       return u2.getalbumByName(name);
+
+
+
+    }
+
+
+
+
+
+
+/*
+
+
+
+    @GetMapping("/find")
+    public Album getByName() {
+        return u2.getAlbum();
+
+    }
+
+
+*/
+}
---
+++ src/main/java/com/example/getmesocialservice/userrepo/repoistery.java
@@ -0,0 +1,10 @@
+package com.example.getmesocialservice.userrepo;
+
+import com.example.getmesocialservice.model.Album;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface repoistery extends MongoRepository<Album, String> {
+     List<Album> findAllByName(String name);
+}
